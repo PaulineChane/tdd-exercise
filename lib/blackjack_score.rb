4f6d3ec27f4f07f8fef14b41ba479a176b7f88f0
Pauline Chane <pauline.chane@gmail.com>
2020-09-14 23:53:11 -0700

    Completed blackjack_score.rb and blackjack_score_test.rb

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,7 +1,61 @@
+# Pauline Chane (@PaulineChane on GitHub)
+# Ada Developers Academy Cohort 14
+# TDD Blackjack Exercise
+# 09/17/2020
+
+# Practice TDD by writing and testing a method to score Blackjack hands (blackjack_score)
+
 # blackjack_score.rb
 
 VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 
 def blackjack_score(hand)
+  # check for valid hand size
+  raise ArgumentError unless VALID_CARDS[0..3].include? hand.length
 
+  # store any aces you come across
+  aces = []
+
+  # since VALID_CARDS is an assortment of strings and numbers, let's convert them into one
+  score_array = hand.map do |card|
+    # check for valid card
+    raise ArgumentError unless VALID_CARDS.include? card
+
+    # log aces for later, but you can add the minimum value
+    if card == "Ace"
+      # store in an array to denote aces but also push the minimum possible value of Ace into score array
+      aces << 1
+      1
+
+      # non-Ace face cards
+    elsif card.is_a? String
+      10
+
+      # number cards
+    else
+      card
+    end
+
+  end
+
+  # store temp sum for now until we can verify any aces
+  score = score_array.reduce(:+)
+
+  # account for aces
+  unless aces.empty?
+    i = 0 # increment variable
+    # add 10 per each ace found (based on aces.length) until score + 10 is as close to 21 before going over
+    # ... or you run out of aces
+    while score + 10 <= 21 && i < aces.length do
+      score += 10
+      i += 1
+    end
+  end
+
+  # sum all cards in hand and check for hands that are too large
+  raise ArgumentError if score > 21
+
+  # hand is definitely valid if it makes it to this point
+  return score
 end
+
--- test/blackjack_score_test.rb
+++ test/blackjack_score_test.rb
@@ -1,3 +1,10 @@
+# Pauline Chane (@PaulineChane on GitHub)
+# Ada Developers Academy Cohort 14
+# TDD Blackjack Exercise
+# 09/17/2020
+
+# Practice TDD by writing tests to validate functionality for a method that scores Blackjack hands (blackjack_score)
+
 require 'minitest'
 require 'minitest/spec'
 require 'minitest/autorun'
@@ -18,26 +25,77 @@ describe 'Blackjack Score' do
     score = blackjack_score(hand)
 
     # Assert <-  You do this part!
-
+    expect(score).must_equal 7
   end
 
   it 'facecards have values calculated correctly' do
+    # Arrange
+    hand = ["Queen", "King"]
 
+    # Act
+    score = blackjack_score(hand)
+
+    # Assert
+    expect(score).must_equal 20
   end
 
   it 'calculates aces as 11 where it does not go over 21' do
+    # Arrange
+    hands = [["Ace", 9],["Ace","Ace","Ace"], [2, 4, 4, "Ace"]]
 
+    # Act
+    score = hands.map{ |hand| blackjack_score(hand) }
+
+    # Assert
+    expect(score).must_equal [20, 13, 21]
   end
 
   it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
+    # Arrange
+    hand = ["Ace", 9, "Queen"]
 
+    # Act
+    score = blackjack_score(hand)
+
+    # Assert
+    expect(score).must_equal 20
   end
 
   it 'raises an ArgumentError for invalid cards' do
+    # Invalid strings
+
+    expect {
+      blackjack_score(["Joker", 2])
+    }.must_raise ArgumentError
+
+    # Invalid numbers
+    expect {
+      blackjack_score([18, 2])
+    }.must_raise ArgumentError
+
+    # Invalid number pt. 2
+    expect {
+      blackjack_score([18.5, "Ace"])
+    }.must_raise ArgumentError
 
   end
 
   it 'raises an ArgumentError for scores over 21' do
-
+    expect {
+      blackjack_score([9, 8, 7])
+    }.must_raise ArgumentError
   end
+
+  it 'raises an ArgumentError for invalid hand size (less than 2 and more than 5 cards)' do
+    # Hand too small
+    expect {
+      blackjack_score([5])
+    }.must_raise ArgumentError
+
+    # Hand too large
+    expect {
+      blackjack_score([2, 3, 4, 5, 6, 7, "King"])
+    }.must_raise ArgumentError
+  end
+
 end
